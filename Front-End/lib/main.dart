97e743ac6e692982cb050efb137e595e95a39115
Nirmal De Silva <nirmal.2019283@iit.ac.lk>
2021-02-23 00:29:46 +0530

    Commit 1 Nirmal

--- Front-End/lib/main.dart
+++ Front-End/lib/main.dart
@@ -2,7 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text('Hello Boys!'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Title Bar 101'),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Center(
+        child: Text(
+          'Hello Boys',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.greenAccent[700],
+            fontFamily: 'Montserrat',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.green,
+      ),
+    ),
   ));
 }
 
