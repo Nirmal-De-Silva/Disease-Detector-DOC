5296c85003c5e2eea182ec750606d5cd32b804cc
Nirmal De Silva <nirmal.2019283@iit.ac.lk>
2021-02-23 03:39:02 +0530

    commit 2 Nirmal

--- Front-End/lib/main.dart
+++ Front-End/lib/main.dart
@@ -1,31 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Title Bar 101'),
-        centerTitle: true,
-        backgroundColor: Colors.greenAccent,
-      ),
-      body: Center(
-        child: Text(
-          'Hello Boys',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.greenAccent[700],
-            fontFamily: 'Montserrat',
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('Click'),
-        backgroundColor: Colors.green,
-      ),
-    ),
+void main() => runApp(MaterialApp(
+    home: Home()
   ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image(
+          image: AssetImage('assets/layout1.png'),
+          height: 2560,
+          width: 1440,
+          fit: BoxFit.fitWidth,
+        )
+
+        // Text(
+        // 'Hello World',
+        // style: TextStyle(
+        //   fontSize: 20.0,
+        //   fontWeight: FontWeight.bold,
+        //   letterSpacing: 2.0,
+        //   color: Colors.greenAccent[700],
+        //   fontFamily: 'Montserrat',
+        // ),
+        // ),
+
+      ),
+
+    );
+  }
 }
+
 
