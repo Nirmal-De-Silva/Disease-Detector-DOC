f4ade2337175934e83eb346284c1b4829089f38b
Pasindu <pasindu.20191167@iit.ac.lk>
2021-02-23 03:55:54 +0530

    Commit 1 pasindu
    create and made changes for the login ui

--- Front-End/lib/main.dart
+++ Front-End/lib/main.dart
@@ -33,4 +33,130 @@ class Home extends StatelessWidget {
   }
 }
 
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/layout2.png'),
+                fit: BoxFit.cover,
+              )),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 300,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Column(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey[500].withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: 'User Name',
+                                  prefix: Padding(
+                                      padding:
+                                      EdgeInsets.symmetric(horizontal: 10)),
+                                  contentPadding:
+                                  const EdgeInsets.symmetric(vertical: 10),
+                                ),
+                                textInputAction: TextInputAction.next,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 12),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[500].withOpacity(0.5),
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'Password',
+                                    prefix: Padding(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 10)),
+                                    contentPadding: const EdgeInsets.symmetric(
+                                        vertical: 10),
+                                  ),
+                                  obscureText: true,
+                                  textInputAction: TextInputAction.done,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'Forgot Password?',
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            SizedBox(
+                              height: 100,
+                            ),
+                            Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: FlatButton(
+                                onPressed: () {},
+                                child: Padding(
+                                  padding:
+                                  const EdgeInsets.symmetric(vertical: 16),
+                                  child: Text(
+                                    'Login',
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 100,
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom:
+                                    BorderSide(color: Colors.black12, width: 1),
+                                  )),
+                              child: Text(
+                                'CreateNewAccount',
+                              ),
+                            ),
+                            SizedBox(
+                              height: 50,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 
