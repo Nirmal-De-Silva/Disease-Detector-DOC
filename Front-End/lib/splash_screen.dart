d406562db0ade2906fa3d7b1ae52276d973028ce
Nirmal De Silva <nirmal.2019283@iit.ac.lk>
2021-02-23 03:59:35 +0530

    Commit 3 Nirmal

---
+++ Front-End/lib/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Home()
+  ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image(
+          image: AssetImage('assets/layout1.png'),
+          height: 2560,
+          width: 1440,
+          fit: BoxFit.fitWidth,
+        )
+
+        // Text(
+        // 'Hello World',
+        // style: TextStyle(
+        //   fontSize: 20.0,
+        //   fontWeight: FontWeight.bold,
+        //   letterSpacing: 2.0,
+        //   color: Colors.greenAccent[700],
+        //   fontFamily: 'Montserrat',
+        // ),
+        // ),
+
+      ),
+
+    );
+  }
+}
+
+
