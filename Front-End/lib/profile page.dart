c925ce6897651a0a28acab5a423b986ebf181a81
Senith-Pinsara <senith.20191175@iit.ac.lk>
2021-03-02 00:25:35 +0530

    Commit 3 Senith
    Final Profile Page

--- Front-End/lib/profile page.dart
+++ Front-End/lib/profile page.dart
@@ -1,16 +1,119 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+void main() => runApp(MaterialApp(
+    home: profile_page()
+));
+
+class profile_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets\layout4.png'),
-          fit: BoxFit.cover,
-          colorFilter: ColorFilter.mode(Colors.black45,BlendMode.darken),
-      ),
-      ),
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/layout4.png'),
+                fit: BoxFit.fill,
+              )),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
+            children: [
+              SizedBox(
+                height: 150,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[500].withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                        ),
+
+                        Padding(
+
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey[500].withOpacity(0.5),
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'Check Up Type',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40,color: Colors.green),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: 200,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: FlatButton(
+                              onPressed: () {},
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 20),
+                                child: Text(
+                                  'Eye Related',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: FlatButton(
+                              onPressed: () {},
+                              child: Padding(
+                                padding:const EdgeInsets.symmetric(vertical: 20),
+                                child: Text(
+                                  'Other',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100,
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
+
+
+
