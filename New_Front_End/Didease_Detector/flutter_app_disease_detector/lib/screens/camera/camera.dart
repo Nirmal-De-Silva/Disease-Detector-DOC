df35a3da633e79e8098b871a6598918a8ff1e491
Senith-Pinsara <senith.20191175@iit.ac.lk>
2021-04-20 11:19:08 +0530

    Camera Access
    Give Camera access to App

---
+++ New_Front_End/Didease_Detector/flutter_app_disease_detector/lib/screens/camera/camera.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart  ';
+import 'package:flutter/material.dart';
+
+class Camera extends StatefulWidget {
+  @override
+  _CameraState createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+
+  File _image;
+  final imagePicker = ImagePicker();
+
+  Future getImage() async{
+    final image = await imagePicker.getImage(source: ImageSource.camera);
+    setState((){
+      _image = File(image.path);
+      print(_image);
+      print("...................................");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/layout2.png'),
+            
+            fit: BoxFit.cover,          
+            
+          ),
+          ),
+
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+          title: Text('Doc Disease'),
+          backgroundColor: Colors.green[500],
+          elevation: 0.0,
+          actions: <Widget>[
+            
+          ],
+
+          ),
+          body: Center(
+            child: _image == null ? Text("No Imge Selected") : Image.file(_image,width: 400, height: 350,),),
+            
+            floatingActionButton: FloatingActionButton(
+              onPressed: 
+              getImage,
+              
+              backgroundColor: Colors.white,
+              child : Icon(
+                Icons.camera_alt,
+                color: Colors.green,),
+              
+            ),
+          ),
+          
+      
+    );
+  }
+}
--- New_Front_End/Didease_Detector/flutter_app_disease_detector/lib/screens/home/home.dart
+++ New_Front_End/Didease_Detector/flutter_app_disease_detector/lib/screens/home/home.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_disease_detector/screens/camera/camera.dart';
 import 'package:flutter_app_disease_detector/services/auth.dart';
+import 'dart:io';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
 
-  final AuthService _auth = AuthService();
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  final AuthService _auth = AuthService();  
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +21,7 @@ class Home extends StatelessWidget {
           image: DecorationImage(
             image: AssetImage('assets/layout2.png'),
             
-            fit: BoxFit.cover,
-          
+            fit: BoxFit.cover,          
             
           ),
           ),
@@ -33,6 +41,53 @@ class Home extends StatelessWidget {
             )
           ],
 
+          ),
+          body: Container(         
+            
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0), 
+            
+            child: Form(              
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 120.0),
+                  
+                  Text("Checkup Type",style: TextStyle(color: Colors.green, fontSize: 40.0),),
+                  SizedBox(height: 150.0,),
+                  RaisedButton.icon(
+                    icon: Icon((Icons.camera_alt)),                                        
+                    padding: EdgeInsets.fromLTRB(77.5,15,77.5,10),
+                    color: Colors.green[400],
+                    label: Text(
+                      'Eye Related',style: TextStyle(color: Colors.white),
+                    ),
+                    
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                    onPressed: () {  
+                      Navigator.push(context,
+                      MaterialPageRoute(builder: (context)=> Camera()),);;
+                     
+                     
+                    },
+                  ),
+                     SizedBox(height: 20.0),
+                  RaisedButton.icon(
+                    icon: Icon(Icons.chat),
+                    padding: EdgeInsets.fromLTRB(96.5,15,96.5,15),
+                    color: Colors.green[400],
+                    label: Text(
+                      'Other',style: TextStyle(color: Colors.white),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                    onPressed: () async{                    
+                     
+                     
+                    },
+                  ),
+                ],
+            ),
+          ),
           ),      
       ),
     );
