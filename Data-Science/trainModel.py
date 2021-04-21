ce20e628d734da92ad5ea8adaff7e7da144394c4
hpjjayara <63858002+hpjjayara@users.noreply.github.com>
2021-04-22 03:36:23 +0530

    Train the eye model

---
+++ Data-Science/trainModel.py
@@ -0,0 +1,78 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.constraints import maxnorm
+from keras.utils import np_utils
+import pickle
+from tensorflow.python.keras import regularizers
+from tensorflow.python.keras.layers import Activation
+from sklearn.model_selection import train_test_split
+
+pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle", "rb")
+y = pickle.load(pickle_in)
+
+
+X = X.astype('float32')
+X = X/255.0
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=33)
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+# Create the model
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), input_shape=X_train.shape[1:], padding='same'))
+model.add(Activation('relu'))
+model.add(Dropout(0.2)) # not overfit
+# model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+# model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+# model.add(BatchNormalization())
+
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+# model.add(BatchNormalization())
+
+model.add(Flatten())
+model.add(Dropout(0.2))
+
+model.add(Dense(256, kernel_regularizer=regularizers.l2(0.0001), kernel_constraint=maxnorm(3)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+# model.add(BatchNormalization())
+model.add(Dense(128, kernel_regularizer=regularizers.l2(0.0001), kernel_constraint=maxnorm(3)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+# model.add(BatchNormalization())
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+epochs = 25
+optimizer = 'Adam'
+
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+print(model.summary())
+
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=64)
+
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+model.save('Eye-model')
