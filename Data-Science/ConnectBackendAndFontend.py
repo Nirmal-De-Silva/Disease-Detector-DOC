64fa055aae77467b722a9480c31002f875f9040c
hpjjayara <63858002+hpjjayara@users.noreply.github.com>
2021-04-23 04:07:17 +0530

    Create backend and frontend connected python file and create functions for image processing part

---
+++ Data-Science/ConnectBackendAndFontend.py
@@ -0,0 +1,42 @@
+import os
+from urllib import request
+from flask import Flask, flash, request, redirect, url_for, render_template, jsonify
+from werkzeug.utils import secure_filename, redirect
+
+app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+# function to allowed file
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'chat': 'Hello Chatbot'})
+
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    filePath = ""
+    if 'file' not in request.files:
+        print('No file part')
+        return filePath
+    file = request.files['file'] #get files, key equel to "file"
+    if file.filename == '': #file name equel to empty
+        print('No image selected for uploading')
+        return filePath
+    if file and allowed_file(file.filename): #get value(aploaded image)
+        filename = "uploaded_image.jpg"
+        filePath = os.path.join(os.getcwd(), "images", filename)
+        file.save(filePath)
+        print('Image successfully uploaded and displayed below')
+        return filePath
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg, gif')
+        return filePath
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host='127.0.0.1', port=5003)
