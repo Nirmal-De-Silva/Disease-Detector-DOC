ff5ac74232b380398ab1f57109b4e1e67658f805
hpjjayara <63858002+hpjjayara@users.noreply.github.com>
2021-04-23 04:02:22 +0530

    change file path and upload image path

--- Data-Science/predictDisease.py
+++ Data-Science/predictDisease.py
@@ -9,28 +9,21 @@ import operator
 #               'Internal Hordeolum', 'Keratitis', 'Ocular Herpes', 'Orbital cellulitis', 'Preseptal Cellulitis',
 #               'strabismus', 'Stye']
 
-filepath = "F:\Academic\SPGP\Test_Deasese"
+filepath = "images"
 
-# # DATADIRECTORY = "F:\Academic\SPGP\Test_Deasese"
-# # # CATEGORIES = ['Blepharitis', 'Cellulitis eye']
-CATEGORIES = ['Cataract','Chalazion', 'Keratitis','Normal eye','Stye' , 'Uveitis' ]
-
-
-
-
+CATEGORIES = ['Cataract', 'Chalazion', 'Keratitis', 'Normal eye', 'Stye', 'Uveitis']
 
 
 def prepare(filepath):
-
     IMG_SIZE = 28  # 50 in txt-based
     img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))/255.0
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE)) / 255.0
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
 
-model = tf.keras.models.load_model("Eye-model1")
+model = tf.keras.models.load_model("Eye-model")
 
-prediction = model.predict([prepare('F:\Academic\SPGP\Test_Deasese\\o3.jpg')])
+prediction = model.predict([prepare('images/uploaded_image.jpg')])
 print(prediction)  # will be a list in a list.
 result = np.argmax(prediction)
 print(result)
