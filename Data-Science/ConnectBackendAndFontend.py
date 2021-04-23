7da9b86cbc290c66a443e71c696a083f94c77e65
hpjjayara <63858002+hpjjayara@users.noreply.github.com>
2021-04-23 06:12:00 +0530

    write API to predict eye disease

--- Data-Science/ConnectBackendAndFontend.py
+++ Data-Science/ConnectBackendAndFontend.py
@@ -2,6 +2,7 @@ import os
 from urllib import request
 from flask import Flask, flash, request, redirect, url_for, render_template, jsonify
 from werkzeug.utils import secure_filename, redirect
+from predictDisease import prepare, predict
 
 app = Flask(__name__)
 
@@ -26,16 +27,19 @@ def upload_image():
     file = request.files['file'] #get files, key equel to "file"
     if file.filename == '': #file name equel to empty
         print('No image selected for uploading')
-        return filePath
+        output = 'No image selected for uploading'
+        return output
     if file and allowed_file(file.filename): #get value(aploaded image)
         filename = "uploaded_image.jpg"
         filePath = os.path.join(os.getcwd(), "images", filename)
         file.save(filePath)
         print('Image successfully uploaded and displayed below')
-        return filePath
+        output = predict()
+        return output
     else:
         flash('Allowed image types are -> png, jpg, jpeg, gif')
-        return filePath
+        output = 'Allowed image types are -> png, jpg, jpeg, gif'
+        return output
 
 
 if __name__ == "__main__":
